2ab19240692c1809b7c8e33c213050722600cf00
Dan David <dandavidmy@gmail.com>
2025-07-01 04:56:41 +0000

    fix

--- src/app/staff/layout.tsx
+++ src/app/staff/layout.tsx
@@ -1,4 +1,6 @@
 
+"use client";
+
 import type { ReactNode } from "react";
 import { AppShell } from "@/components/layout/app-shell"; 
 import { SidebarNav, type NavItemGroup } from "@/components/layout/sidebar-nav"; 
